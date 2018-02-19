82b36bd2c1fdf855efa76c8794d520a343b93dfc
YWHo <YWHo@users.noreply.github.com>
2018-02-19 15:51:16 +1300

    enable Hello World React page

---
+++ client/components/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+// import ReactDOM from 'react-dom'
+//import { HashRouter, Route, Redirect } from 'react-router-dom'
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  
+  render() {
+
+    return (
+    //   <HashRouter>
+        <div>
+          <h1>Hello World </h1>
+        </div>
+    //   </HashRouter>
+    )
+  
+  }
+}
+
+export default App
