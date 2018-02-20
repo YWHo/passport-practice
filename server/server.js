bf00cb388fb9fabe29cd4b05e40138f53f711234
YWHo <YWHo@users.noreply.github.com>
2018-02-21 12:35:15 +1300

    add passport and session package

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
+const passport = require('passport')
+const passportLocal = require('passport-local')
+const expressSession = require('express-session')
 
 const server = express()
 
-server.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(expressSession({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+server.use(passport.initialize())
+server.use(passport.session())
 server.use(express.static(path.join(__dirname, '../public')))
 
 
